322cb58302645a2922fda3e100b452bcb4f84cda
Ronald Moesbergen <github@rmoesbergen.nl>
2020-05-25 16:28:12 +0200

    Add chmod+x

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ sudo apt-get -yy update
 sudo apt-get -yy install python3-requests
 
 wget https://raw.githubusercontent.com/rmoesbergen/teletekst/master/teletekst.py -O /home/pi/teletekst.py
+chmod +x /home/pi/teletekst.py
+
 # Install cron job a 1 minute past midnight
 echo "1 0 * * * pi /home/pi/teletekst.py '/home/pi/teletekst-%m.csv'" | sudo tee /etc/cron.d/teletekst
